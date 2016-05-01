f49b12db0984bba3cf748492775a00c1d8a4e093
Jasper van Esveld <Jasper van Esveld>
2016-05-01 14:33:25 +0200

    Added an example test
     - There is a test that succeeds and one that fails as a refrence for later and to test Unity Cloud Build
     - Updated .gitignore with a standard unity3d Setup found on the internet
     - Changed project setting to serialize assets as text for clearer changes in commits

---
+++ Assets/Test/Editor/TestScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+/// <summary>
+/// Example test script showing a failing and succeeding test.
+/// </summary>
+public class EditorTest {
+
+    [Test]
+    public void FailingTest()
+    {
+        var gameObject = new GameObject();
+        var newGameObjectName = "Fail";
+        gameObject.name = newGameObjectName;
+        Assert.AreEqual("My game object", gameObject.name);
+    }
+
+    [Test]
+    public void SucceedingTest()
+    {   
+        var gameObject = new GameObject();
+        var newGameObjectName = "My game object";
+        gameObject.name = newGameObjectName;
+        Assert.AreEqual("My game object", gameObject.name);
+    }
+}

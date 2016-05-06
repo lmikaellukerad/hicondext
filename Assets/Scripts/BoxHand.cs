9166b697e5e529d0a3f6769e13c9a16f45a7eb7b
Luke Prananta <mikaellukeradheyaprananta@student.tudelft.nl>
2016-05-06 14:18:17 +0200

    Box Hand Representation Scripts added for temporary hand and behaviour added when tracking lost

---
+++ Assets/Scripts/BoxHand.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+using System;
+
+/**
+ * Box Hand model for testing
+ * extends HandModel
+ * 
+ */
+namespace Leap.Unity
+{
+    public class BoxHand : HandModel
+    {
+        public override ModelType HandModelType
+        {
+            get
+            {
+                return ModelType.Graphics;
+            }
+        }
+
+        public Quaternion Reorientation()
+        {
+            return Quaternion.Inverse(Quaternion.LookRotation(Vector3.forward, Vector3.up));
+        }
+
+        public override void UpdateHand()
+        {
+            if (palm != null)
+            {
+                palm.position = GetPalmPosition();
+                palm.rotation = GetPalmRotation() * Reorientation();
+            }
+
+            if (forearm != null)
+                forearm.rotation = GetArmRotation() * Reorientation();
+        }
+    }
+}

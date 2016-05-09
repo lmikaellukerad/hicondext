a5479f2644d425b390239424491c8a8686147e97
Jasper van Esveld <J.M.vanEsveld@student.tudelft.nl>
2016-05-10 01:11:44 +0200

    Added rigged hand prefab
    Finally working properly don't get distracted by the base pose though

--- Assets/Hardware Plugins/LeapMotion/Scripts/Hands/RiggedHand.cs
+++ Assets/Hardware Plugins/LeapMotion/Scripts/Hands/RiggedHand.cs
@@ -8,40 +8,59 @@ using UnityEngine;
 using System.Collections;
 using Leap;
 
-namespace Leap.Unity {
-  // Class to setup a rigged hand based on a model.
-  public class RiggedHand : HandModel {
-    public override ModelType HandModelType {
-      get {
-        return ModelType.Graphics;
-      }
-    }
-    public Vector3 modelFingerPointing = Vector3.forward;
-    public Vector3 modelPalmFacing = -Vector3.up;
-
-    public override void InitHand() {
-      UpdateHand();
-    }
-
-    public Quaternion Reorientation() {
-      return Quaternion.Inverse(Quaternion.LookRotation(modelFingerPointing, -modelPalmFacing));
-    }
-
-    public override void UpdateHand() {
-      if (palm != null) {
-        palm.position = GetPalmPosition();
-        palm.rotation = GetPalmRotation() * Reorientation();
-      }
-
-      if (forearm != null)
-        forearm.rotation = GetArmRotation() * Reorientation();
-
-      for (int i = 0; i < fingers.Length; ++i) {
-        if (fingers[i] != null) {
-          fingers[i].fingerType = (Finger.FingerType)i;
-          fingers[i].UpdateFinger();
+namespace Leap.Unity
+{
+    // Class to setup a rigged hand based on a model.
+    public class RiggedHand : HandModel
+    {
+        public override ModelType HandModelType
+        {
+            get
+            {
+                return ModelType.Graphics;
+            }
         }
-      }
+        public Vector3 modelFingerPointing = Vector3.forward;
+        public Vector3 modelPalmFacing = -Vector3.up;
+
+        public override void InitHand()
+        {
+            UpdateHand();
+        }
+
+        public Quaternion Reorientation()
+        {
+            return Quaternion.Inverse(Quaternion.LookRotation(modelFingerPointing, -modelPalmFacing));
+        }
+
+        public override void UpdateHand()
+        {
+            if (IsTracked)
+            {
+                if (palm != null)
+                {
+                    palm.position = GetPalmPosition();
+                    palm.rotation = GetPalmRotation() * Reorientation();
+                }
+
+                if (forearm != null)
+                {
+                    if (IsTracked)
+                    {
+                        Quaternion armRotation = GetArmRotation();
+                        forearm.rotation = Quaternion.Euler(0f, 180f, 0f) * (new Quaternion(-armRotation.x, armRotation.y, -armRotation.z, armRotation.w) * Reorientation());
+                    }
+                }
+
+                for (int i = 0; i < fingers.Length; ++i)
+                {
+                    if (fingers[i] != null)
+                    {
+                        fingers[i].fingerType = (Finger.FingerType)i;
+                        fingers[i].UpdateFinger();
+                    }
+                }
+            }
+        }
     }
-  } 
 }

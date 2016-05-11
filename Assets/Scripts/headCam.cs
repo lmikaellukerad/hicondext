99b659efa7e6e13674937cdef7a8ed301945b7f6
mtavasszy <M.H.tavasszy@student.tudelft.nl>
2016-05-11 16:28:38 +0200

    add support for Kinect
    the camera is now linked to the head of the kinect avatar. This means that head movement and rotation is followed by the camera within Unity.

---
+++ Assets/Scripts/headCam.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class headCam : MonoBehaviour {
+    public GameObject Head;
+
+    // Use this for initialization
+    void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Head != null)
+        {
+            transform.position = Head.GetComponent<Transform>().position;
+            transform.rotation = Head.GetComponent<Transform>().rotation;
+        }
+    }
+}

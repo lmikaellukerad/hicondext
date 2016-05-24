5819e1658dd339a7dc4bd42398e04db64ea7573f
Luke Prananta <mikaellukeradheyaprananta@student.tudelft.nl>
2016-05-24 12:07:47 +0200

    FingerTipCollider script added
    Stylecop proof and ghost doc'd

---
+++ Assets/Scripts/FingerTipCollider.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// This code is used to detect any collisions between the fingertips and other objects.
+/// </summary>
+public class FingerTipCollider : MonoBehaviour
+{
+    public Transform[] FingerTips = new Transform[4];
+
+    public Transform Thumb;
+    
+    /// <summary>
+    /// Starts this instance.
+    /// </summary>
+    private void Start()
+    {
+        this.Initialize();
+    }
+
+    /// <summary>
+    /// This method is used to add a SphereCollider to a GameObject.
+    /// </summary>
+    /// <param name="obj">The object to add a SphereCollider to.</param>
+    private void AddCollider(GameObject obj)
+    {
+        SphereCollider s = obj.AddComponent<SphereCollider>();
+        s.radius = 0.025f;
+    }
+
+    /// <summary>
+    /// Initializes this instance.
+    /// </summary>
+    private void Initialize()
+    {
+        foreach (Transform t in this.FingerTips)
+        {
+            this.AddCollider(t.gameObject);
+        }
+
+        if (this.Thumb != null)
+        {
+            this.AddCollider(this.Thumb.gameObject);
+        }
+    }
+
+    /// <summary>
+    /// Updates this instance for every frame.
+    /// </summary>
+    private void Update()
+    {
+    }
+}

3889cbc7f6cb9eacb0453a541306a0794ac20c2f
mtavasszy <matt.tavasszy@gmail.com>
2016-06-02 11:17:47 +0200

    implement physics-based grabbing detection
    not tested yet

---
+++ Assets/Scripts/PhysicsGrabBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public abstract class PhysicsGrabBehaviour : MonoBehaviour
+{
+    public PhysicsGrabState State
+    {
+        get;
+        set;
+    }
+
+    public Vector3 PinchPosition
+    {
+        get;
+        protected set;
+    }
+
+    public GameObject GrabbedItem
+    {
+        get;
+        protected set;
+    }
+
+    public abstract void Initialize();
+
+    public abstract void CheckGrab();
+
+    public abstract void CheckRelease();
+
+    public abstract void OnHold();
+
+    public abstract void OnRelease();
+
+    public abstract bool DetectGrab(Transform[] fingers);
+
+    public abstract bool DetectRelease(Transform thumb);
+}
---
+++ Assets/Scripts/PhysicsGrabStates/PhysicsGrabState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// Author: Luke
+/// Abstract class GrabState for defining the state of a grab.
+/// </summary>
+public abstract class PhysicsGrabState : ScriptableObject
+{
+    /// <summary>
+    /// Handle the state of the grab based on the current GrabBehavior properties.
+    /// </summary>
+    /// <param name="context">The GrabBehavior object that provides context for the state.</param>
+    public abstract void Handle(PhysicsGrabBehaviour context);
+
+    internal void Handle(PhysicsGrab physicsGrab)
+    {
+        throw new NotImplementedException();
+    }
+}

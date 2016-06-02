5e97b7ab07fa747293eb889589174ac8ebf44da9
Jasper van Esveld <J.M.vanEsveld@student.tudelft.nl>
2016-06-02 13:43:06 +0200

    OnGrab adds max bend to prevent clipping inside the object

---
+++ Assets/Scripts/HandSimulator/FingerLimit.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+class FingerLimit : ScriptableObject
+{
+
+    public int Finger
+    {
+        get;
+        private set;
+    }
+
+    public float MinLimit
+    {
+        get;
+        private set;
+    }
+
+    public float MaxLimit
+    {
+        get;
+        private set;
+    }
+    private float maxLimit;
+
+    public FingerLimit(int finger, float min, float max)
+    {
+        this.Finger = finger;
+        this.MinLimit = min;
+        this.MaxLimit = max;
+    }
+}

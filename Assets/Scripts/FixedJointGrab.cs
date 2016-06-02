d78504f7e015fc51394db64a216b8391a1001b39
Luke Prananta <mikaellukeradheyaprananta@student.tudelft.nl>
2016-06-02 12:58:07 +0200

    Fix for manusgrab test

--- Assets/Scripts/FixedJointGrab.cs
+++ Assets/Scripts/FixedJointGrab.cs
@@ -124,8 +124,6 @@ public class FixedJointGrab : GrabBehaviour
                 this.GrabbedObject.GetComponent<Collider>().enabled = false;
             }
         }
-
-        this.RecognizeGesture();
     }
 
     /// <summary>

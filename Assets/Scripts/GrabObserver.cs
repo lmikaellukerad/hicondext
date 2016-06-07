a6f0d2ca905b8f1ab88822ec84b31ff054b410ac
Jasper van Esveld <J.M.vanEsveld@student.tudelft.nl>
2016-06-07 13:35:00 +0200

    Implemented CheckGrab and removed some more silly names
    UpdateObject uses average finger position of the grabbing fingers

--- Assets/Scripts/GrabObserver.cs
+++ Assets/Scripts/GrabObserver.cs
@@ -23,11 +23,11 @@ public class GrabObserver
     /// <param name="right">The right hand model.</param>
     /// <param name="left">The left hand model.</param>
     /// <param name="obj">The object that is currently grabbed.</param>
-    public GrabObserver(GrabSubjectBehaviour subject, HandModel right, HandModel left, GameObject obj)
+    public GrabObserver(GrabSubjectBehaviour subject, HandModel left, HandModel right, GameObject obj)
     {
         this.sub = subject;
         this.rightHand = right;
-        this.leftHand = right;
+        this.leftHand = left;
         this.allFingerTips = this.leftHand.fingers.Cast<Transform>().ToList<Transform>();
         this.allFingerTips.AddRange(this.rightHand.fingers.Cast<Transform>().ToList<Transform>());
         this.obj = obj;
@@ -58,6 +58,48 @@ public class GrabObserver
     /// <returns>True if the object is still grabbed</returns>
     private bool CheckGrabbed()
     {
+        this.grabbingFingerTips.Clear();
+        List<Vector3> normals = new List<Vector3>();
+        for (int i = 0; i < this.allFingerTips.Count; i++)
+        {
+            DetectCollision tip = this.allFingerTips[i].GetComponent<DetectCollision>();
+            if (tip.Collided && tip.Object.gameObject == this.obj)
+            {
+                this.grabbingFingerTips.Add(this.allFingerTips[i]);
+                normals.Add(Vector3.Normalize(tip.Object.contacts[0].normal));
+            }
+        }
+
+        if (this.HasOpposites(normals))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// Determines whether the specified vectors has opposites.
+    /// </summary>
+    /// <param name="vectors">The vectors.</param>
+    /// <returns>True if the list has two opposite vectors</returns>
+    private bool HasOpposites(List<Vector3> vectors)
+    {
+        foreach (Vector3 n in vectors)
+        {
+            foreach (Vector3 m in vectors)
+            {
+                if (n != m)
+                {
+                    float d = Vector3.Dot(n, m);
+                    if (d < 0.5)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+
         return false;
     }
 
@@ -66,5 +108,22 @@ public class GrabObserver
     /// </summary>
     private void UpdateObject()
     {
+        this.obj.transform.position = this.AveragePosition(this.grabbingFingerTips);
+    }
+
+    /// <summary>
+    /// Averages the position of the transforms.
+    /// </summary>
+    /// <param name="transforms">The transforms.</param>
+    /// <returns>The average position vector</returns>
+    private Vector3 AveragePosition(List<Transform> transforms)
+    {
+        Vector3 total = Vector3.zero;
+        foreach (Transform trans in transforms)
+        {
+            total += trans.position;
+        }
+
+        return total / transforms.Count;
     }
 }

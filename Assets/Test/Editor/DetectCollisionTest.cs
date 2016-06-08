a466153eb4a6243565f42e8cf500523bc831b88b
Arjan <a.j.vanschendel@student.tudelft.nl>
2016-06-08 13:46:59 +0200

    Added DetectCollisionTest

---
+++ Assets/Test/Editor/DetectCollisionTest.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+public class DetectCollisionTest
+{
+
+    private GameObject testObj;
+
+    [SetUp]
+    public void setup()
+    {
+        testObj = new GameObject("testobj");
+        testObj.AddComponent<DetectCollision>();
+    }
+
+    [Test]
+    public void NotCollidedTest()
+    {
+        Assert.False(testObj.GetComponent<DetectCollision>().Collided);
+    }
+
+    [Test]
+    public void CollidedBooleanTest()
+    {
+        Collision c = new Collision();
+        testObj.GetComponent<DetectCollision>().CollisionEnterSimulator(c);
+        Assert.True(testObj.GetComponent<DetectCollision>().Collided);
+    }
+
+    [Test]
+    public void CollidedObjectTest()
+    {
+        Collision c = new Collision();
+        testObj.GetComponent<DetectCollision>().CollisionEnterSimulator(c);
+        Assert.AreEqual(testObj.GetComponent<DetectCollision>().Object, c);
+    }
+
+    [Test]
+    public void ExitBooleanTest()
+    {
+        Collision c = new Collision();
+        testObj.GetComponent<DetectCollision>().CollisionExitSimulator(c);
+        Assert.False(testObj.GetComponent<DetectCollision>().Collided);
+    }
+
+    [Test]
+    public void ExitObjectTest()
+    {
+        Collision c = new Collision();
+        testObj.GetComponent<DetectCollision>().CollisionExitSimulator(c);
+        Assert.AreEqual(testObj.GetComponent<DetectCollision>().Object, null);
+    }
+}
